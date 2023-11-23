723705a6752000100bd76bada1c486167c001459
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-11-23 23:15:21 +0100

    add sout 5

--- src/B.java
+++ src/B.java
@@ -2,5 +2,9 @@ public class B {
     public static void main(String[] args) {
         System.out.println();
         System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
